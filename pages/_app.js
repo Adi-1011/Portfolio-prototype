c1f35c12d3cd31b12fe0f2678c68f97542127af0
Adi-1011 <sonu10112002@gmail.com>
2025-08-26 01:36:41 +0530

    Initial commit - cloned and configured base project

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/index.css'
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import 'tailwindcss/tailwind.css';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const trackingId = process.env.NEXT_PUBLIC_TRACKING_ID;
+
+    if (trackingId) {
+      ReactGA.initialize(trackingId);
+      ReactGA.send("pageview");
+    } else {
+      console.warn("GA tracking ID not found, skipping initialization.");
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
